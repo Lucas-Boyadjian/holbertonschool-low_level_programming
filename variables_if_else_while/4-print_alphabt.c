75989fe5ef8d200b0ef62f4a71cc683613fd764c
Yadjian92 <10462@holbertonstudents.com>
2025-02-17 14:44:54 +0000

    no_q_no_e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - Print th alphabet without  q and e
+ * main - Prints if number is positive, zero or negative
  *
  * Return: Always (Success)
  */
-	int main(void)
+int main(void)
 {
 	char alphabet;
-
-	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
-{
-	if (alphabet == 'e' || alphabet == 'q')
-	continue;
-
-	putchar(alphabet);
-}
-
-	putchar('\n');
-	return (0);
+		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		if (alphabet == 'e' || alphabet == 'q')
+			continue;
+		putchar(alphabet);
+	}
+	putchar ('\n');
+return (0);
 }
