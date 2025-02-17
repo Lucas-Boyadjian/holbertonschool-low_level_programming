6e99d268a7ed82dd84b7c79d5f05d6851600714c
Yadjian92 <10462@holbertonstudents.com>
2025-02-17 14:34:29 +0000

    Alphabet_sans_q_et_e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,16 @@
  */
 	int main(void)
 {
-
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet < 'z' ; alphabet++)
-	{
-	putchar(alphabet);
-
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
 	if (alphabet == 'e' || alphabet == 'q')
 	continue;
-	}
+
+	putchar(alphabet);
+}
+
 	putchar('\n');
 	return (0);
 }
