b6e4db326ca6c1b754586c7148fdf7602e797a0a
Yadjian92 <10462@holbertonstudents.com>
2025-02-17 14:56:06 +0000

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Numbers 0 to 10
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int number;
+	for (number = 0 ; number < 10  ; number++)
+	printf("%d\n", number);
+return (0);
+}
