74f41edfa30fff4fb26326337fca71ffe0583b01
Yadjian92 <10462@holbertonstudents.com>
2025-02-18 14:36:04 +0000

    printalphax10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
-a
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet x10
+ */
+
+void print_alphabet_x10(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	_putchar(alphabet);
+}
+	_putchar('\n');
+}
+}
