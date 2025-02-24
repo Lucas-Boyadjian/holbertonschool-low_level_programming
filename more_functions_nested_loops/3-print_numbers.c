b62a0468063845d830f26dd723f43f377b6f99e0
Yadjian92 <10462@holbertonstudents.com>
2025-02-24 10:56:47 +0000

    Print_numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_numbers - Print numbers 0 to 9
+ * Return: Always (Succes)
+ */
+
+void print_numbers(void)
+{
+	char number;
+		for (number = 48; number <= 57; number++)
+		{
+		_putchar(number);
+		_putchar('\n');
+		}
+}
