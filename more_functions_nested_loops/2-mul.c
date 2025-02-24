a0f13141f7e8472854d3c599b7525e39b358fdac
Yadjian92 <10462@holbertonstudents.com>
2025-02-24 09:57:06 +0000

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * mul - function
+ * @a: Variable a
+ * @b: Variable b
+ * Return: result a*b
+ */
+
+int mul(int a, int b)
+
+{
+return (a * b);
+}
