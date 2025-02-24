42c2003ca6b2bf1a5b3242d75a91cc2b1d3c23b0
Yadjian92 <10462@holbertonstudents.com>
2025-02-24 23:13:33 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_diagonal - Print '\'
+ * @n: Print numbers of '\'
+ * Return: Always (Succes)
+ */
+void print_diagonal(int n)
+
+{
+int a;
+int b;
+int c;
+        if (size <= 0)
+        {
+        _putchar('\n');
+        }
+a = 0;
+	for (a = 0; a < size; a++)
+	for (c = 0; < size; c--)
+{
+_putchar('#');
+}
+for (b = 0; b < size; b++)
+{
+_putchar(' ');
+}
+					}
+	}
+}
