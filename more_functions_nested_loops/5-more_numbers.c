865a6ef2931c959bf0e240a885724be66f641d9c
Yadjian92 <10462@holbertonstudents.com>
2025-02-24 11:32:11 +0000

    try

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_most_numbers - Print numbers 0 to 9 but not 2 and 4
+ * Return: Always (Succes)
+ */
+
+void print_most_numbers(void)
+
+{
+int number;
+	for (number = '0'; number <= 14; number++)
+	_putchar(number);
+		{
+		for (number = '0'; number <= 10; number++)
+		_putchar('\n');
+		}
+}
