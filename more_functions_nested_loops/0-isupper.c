9d481f7b0287abea520f62b09dbe28b1e4c47c00
Yadjian92 <10462@holbertonstudents.com>
2025-02-24 09:16:27 +0000

    AtoZ

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * int_isupper - Value A to Z
+ * @c: Character
+ * Return: if c is uppercase return 1 and return 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	if (c >= '65' && c <= '90')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
