92d73a3658a78d2b1b69677781bd614d33d95ade
Yadjian92 <10462@holbertonstudents.com>
2025-02-24 22:24:00 +0000

    FizzandBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,24 @@ for (i = 1; i <= 100; i++)
 {
 	if (i % 3 == 0 && i % 5 == 0)
 	{
-	printf("FizzBuzz");
+		printf("FizzBuzz");
 	}
-		else if (i % 3 == 0)
-		{
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else if (i % 3 == 0)
+	{
 		printf("Fizz");
-		}
-			else if (i % 5 == 0)
-			{
-			printf("Buzz");
-			}
-				else
-				{
-				printf("%d ", i);
-				}
+	}
+	else
+	{
+		printf("%d", i);
+	}
 	if (i < 100)
 	{
-	printf(" ");
+		printf(" ");
 	}
-
 }
 printf("\n");
 return (0);
