ac6454d5cc95c372d04c58060821622eeb889413
Yadjian92 <10462@holbertonstudents.com>
2025-02-25 14:54:53 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strlen - function tant returns the length of a string
+* 
+*
+*/
+
+
+int _strlen(char *s)
+{
+int count = 0;
+
+for (; ; *s != '\0'; count++)
+
