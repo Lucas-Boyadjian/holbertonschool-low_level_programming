20f45b4e232b1b4fc919b6d43d843a6ee59b8d98
Yadjian92 <10462@holbertonstudents.com>
2025-02-26 18:18:36 +0100

    Putsagain

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -13,10 +13,10 @@ void puts2(char *str)
 {
 int i = 0;
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-        if (i % 2 == 0)
-        _putchar(str[i]);
-        }
+		for (i = 0; str[i] != '\0'; i++)
+		{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+		}
 _putchar('\n');
 }
