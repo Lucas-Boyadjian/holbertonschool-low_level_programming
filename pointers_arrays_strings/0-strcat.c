8b85e102fd66c3adb8cb4f91afb83908a003093b
Yadjian92 <10462@holbertonstudents.com>
2025-02-27 21:49:14 +0100

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - Write a function that concatenates two strings.
+ * @src: The source string to be copied.
+ * @dest: The buffer where the string will be copied.
+ * Return: A pointer to the destination string (dest).
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+int a = 0;
+int b = 0;
+
+while (dest[a] != '\0')
+{
+a++;
+}
+
+b = 0;
+
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
