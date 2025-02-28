c0cf484f76f68f5a6b452d35c0e4d3696c4e826b
Yadjian92 <10462@holbertonstudents.com>
2025-02-28 09:38:17 +0100

    reverse_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* reverse_array - print an array of integers
+* @a: an array of integers
+* @n: the number of elements to swap
+*
+* Return: nothing.
+*/
+
+void reverse_array(int *a, int n)
+
+{
+int start = 0;
+int end = n - 1;
+int tmp;
+
+while (start < end)
+{
+tmp = a[start];
+a[start] = a[end];
+a[end] = tmp;
+
+start++;
+end--;
+
+}
+}
