97046f875d96494c4abbfc49a56fafd765063730
Yadjian92 <10462@holbertonstudents.com>
2025-03-01 13:25:15 +0100

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,26 +3,16 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src to dest.
+ * _atoi - 
  *
- * @src: The source string to be copied.
- * @dest: The buffer where the string will be copied.
- * Return: A pointer to the destination string (dest).
+ * 
+ * 
+ * 
  */
 
 int _atoi(char *s)
 
 {
 
-int length = 0;
 
-	while (src[length] != '\0')
-	{
-	dest[length] = src[length];
-	length++;
-		}
-
-dest[length] = '\0';
-
-return (dest);
 }
