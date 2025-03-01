5e02b0b88feaa87ae662d2c944c15186726fc3d2
Yadjian92 <10462@holbertonstudents.com>
2025-03-01 18:59:09 +0100

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,22 +16,18 @@ int _atoi(char *s)
 int result = 0;
 int sign = 1;
 int i = 0;
-
 while (s[i] != '\0')
 	{
-
 	if (s[i] == ' ')
 		{
 		i++;
 		continue;
 		}
-
 	if (s[i] >= 65 && s[i] <= 122)
 		{
 		i++;
 		continue;
 		}
-
 	if (s[i] == '+' || s[i] == '-')
 		{
 		if (s[i] == '-')
@@ -41,19 +37,15 @@ while (s[i] != '\0')
 		i++;
 		continue;
 		}
-
 	if (s[i] >= '0' && s[i] <= '9')
 		{
 		result = result * 10 + (s[i] - '0');
 		}
-
 	else
 		{
 		break;
 		}
-
 	i++;
 	}
 return (result * sign);
-
 }
