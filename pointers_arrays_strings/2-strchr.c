2997040cc2f30497103f7a33fa7f6c141fa187b7
Yadjian92 <10462@holbertonstudents.com>
2025-03-04 23:23:02 +0100

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,14 @@
 */
 
 char *_strchr(char *s, char c)
-
 {
-int i = 0;
-
-while (s[i] != '\0')
-{
-if (s[i] == c)
-return (s + i);
-i++;
-}
-if (c == '\0')
-return (s);
-
-return (NULL);
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+	s++;
+	}
+	if (c == '\0')
+		return (s);
+			return (NULL);
 }
