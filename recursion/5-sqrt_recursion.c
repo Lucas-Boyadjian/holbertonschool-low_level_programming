a1eeec96b2a391752fe800e30430e69dd053c90b
Yadjian92 <10462@holbertonstudents.com>
2025-03-04 21:11:12 +0100

    sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
-*_sqrt_helper -
-*_sqrt_recursion -
+*_sqrt_helper - helper function for calculating the square root
 * @n: the number to find the square root of
 * @number: the current guess for the square root
 * Return: the square root of n if it exists, otherwise -1
@@ -17,6 +16,13 @@ if (number * number == n)
 return (number);
 return (_sqrt_helper(n, number + 1));
 }
+
+/**
+*_sqrt_recursion - Computes the square root of a non-negative integer
+* @n: the number to find the square root of
+* @number: the current guess for the square root
+* Return: the square root of n if it exists, otherwise -1
+*/
 
 int _sqrt_recursion(int n)
 {
