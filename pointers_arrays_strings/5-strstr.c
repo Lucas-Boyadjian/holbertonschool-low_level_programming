49ec929b6d5b48c82b2932f32c188105033e1540
Yadjian92 <10462@holbertonstudents.com>
2025-03-04 09:38:37 +0100

    diagsums

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,29 +12,31 @@
 char *_strstr(char *haystack, char *needle)
 
 {
-
 int a = 0;
 int b = 0;
 
-if (needle[b] == '\0')
+if (needle[0] == '\0')
+{
 return (haystack);
+}
 
 while (haystack[a] != '\0')
 {
 b = 0;
-	if (haystack[a] == needle[b])
-	{
-		while (needle[b] != '\0' && haystack[a + b]
-			!= '\0' && needle[b] == haystack[a + b])
-		{
-			b++;
-		}
-		if (needle[b] == '\0')
-		{
-			return (haystack + a);
-		}
-	a++;
-	}
+if (haystack[a] == needle[b])
+{
+while (needle[b] != '\0' && haystack[a + b] != '\0' &&
+needle[b] == haystack[a + b])
+{
+b++;
+}
+if (needle[b] == '\0')
+{
+return (haystack + a);
+}
+}
+a++;
+}
 return (NULL);
 }
-}
+
