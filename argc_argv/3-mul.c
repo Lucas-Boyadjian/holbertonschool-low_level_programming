0c3098d65340bd669a0ad20998a3c10fe607eb9b
Yadjian92 <10462@holbertonstudents.com>
2025-03-05 13:24:09 +0100

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,11 +11,20 @@
 int main(int argc, char *argv[])
 
 {
-int i;
+int a;
+int b;
+int result;
 
-for (i = 0; i < argc; i++)
+if (argc != 3)
 {
-	printf("%s\n", argv[i]);
-}
+printf("Error\n");
 return (1);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+
+result = a * b;
+printf("%d\n", result);
+return(0);
 }
