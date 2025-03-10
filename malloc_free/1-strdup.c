cf84c00aa9960f6bfea89a353f74383fb79f0c01
Yadjian92 <10462@holbertonstudents.com>
2025-03-10 13:45:38 +0100

    task2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *_strdup - function to duplicate the string
+* @str : String to duplicate
+* Return: return (str_copy)
+*/
+
+char *_strdup(char *str)
+
+{
+int i;
+int size = 0;
+char *str_copy;
+
+
+if (str == NULL)
+{
+	return (NULL);
+}
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+
+	str_copy = malloc((size + 1) * sizeof(char));
+
+if (str_copy == NULL)
+{
+	return (NULL);
+}
+
+	for (i = 0; i <= size; i++)
+	{
+		str_copy[i] = str[i];
+	}
+str_copy[size] = '\0';
+
+return (str_copy);
+}
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *_strdup - function to duplicate the string
+* @str : String to duplicate
+* Return: return (str_copy)
+*/
+
+char *str_concat(char *s1, char *s2)
+
+{
+int i;
+int j;
+
+for ()
+}
