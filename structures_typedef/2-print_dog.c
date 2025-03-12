1f0a59c20ee3b7a1a04d6372ffe44f2f409047bd
Yadjian92 <10462@holbertonstudents.com>
2025-03-12 11:13:02 +0100

    task2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,24 +12,24 @@ if (d == NULL)
 return;
 {
 
-if (d->name != NULL)
-{
-	printf("Name: %s\n", d->name);
-}
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
 	else
 	{
 		printf("Name: (nil)\n");
 	}
 
-if (d->owner != NULL)
-{
-	printf("Owner: %s\n", d->owner);
-}
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 	else
 	{
 		printf("Owner: (nil)\n");
 	}
 
-printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 }
 }
