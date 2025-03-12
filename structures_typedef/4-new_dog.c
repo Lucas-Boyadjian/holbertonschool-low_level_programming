071289e44036477b34fa7724572df6cd8ed29f34
Yadjian92 <10462@holbertonstudents.com>
2025-03-12 16:26:30 +0100

    bettyrelou

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,54 +1,66 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+* _strlen - Retourne la longueur d'une chaîne.
+* @s: Pointeur vers la chaîne à mesurer.
+* Return: La longueur de la chaîne.
+*/
+int _strlen(char *s)
+{
+	int c = 0;
 
- #include <stdio.h>
- #include <stdlib.h>
- #include "dog.h"
- /**
- * *new_dog - Creates a new dog
- * @name: char name
- * @age: float age
- * @owner: char owner
- * Return : return (new_dog_ptr) or NULL if it's fail
- */
+	while (*s++)
+		c++;
+	return (c);
+}
+/**
+* _strcpy - Copie une chaîne de caractères.
+* @src: Chaîne source.
+* @dest: Destination.
+* Return: Pointeur vers la chaîne copiée.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+* new_dog - Crée un nouveau chien et alloue de la mémoire.
+* @name: Nom du chien.
+* @age: Âge du chien.
+* @owner: Nom du propriétaire.
+* Return: Pointeur vers une nouvelle structure dogt ou NULL en cas d'échec.
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int name_length = 0, int owner_length = 0, int i  = 0, int j = 0;
-dog_t *new_dog_ptr;
-if (name == NULL || owner == NULL)
-return (NULL);
-while (name[name_length] != '\0')
-{
-name_length++;
-}
-while (owner[owner_length] != '\0')
-{
-owner_length++;
-}
-new_dog_ptr = malloc(sizeof(dog_t));
-if (new_dog_ptr == NULL)
-return (NULL);
-new_dog_ptr->name = malloc((name_length + 1) * sizeof(char));
-if (new_dog_ptr->name == NULL)
-{
-free(new_dog_ptr);
-return (NULL);
-}
-new_dog_ptr->owner = malloc((owner_length + 1) * sizeof(char));
-if (new_dog_ptr->owner == NULL)
-{
-free(new_dog_ptr->name);
-free(new_dog_ptr);
-return (NULL);
-}
-for (i = 0; i < name_length; i++)
-{
-new_dog_ptr->name[i] = name[i];
-}
-new_dog_ptr->name[name_length] = '\0';
-for (j = 0; j < owner_length; j++)
-{
-new_dog_ptr->owner[j] = owner[j];
-}
-new_dog_ptr->owner[owner_length] = '\0';
-new_dog_ptr->age = age;
-return (new_dog_ptr);
+	dog_t *new_dog_instance;
+
+	new_dog_instance = malloc(sizeof(dog_t));
+	if (new_dog_instance == NULL)
+		return (NULL);
+	new_dog_instance->name = malloc(_strlen(name) + 1);
+	if (new_dog_instance->name == NULL)
+	{
+		free(new_dog_instance);
+		return (NULL);
+	}
+	new_dog_instance->owner = malloc(_strlen(owner) + 1);
+	if (new_dog_instance->owner == NULL)
+	{
+		free(new_dog_instance->name);
+		free(new_dog_instance);
+		return (NULL);
+	}
+	_strcpy(new_dog_instance->name, name);
+	_strcpy(new_dog_instance->owner, owner);
+new_dog_instance->age = age;
+return (new_dog_instance);
 }
