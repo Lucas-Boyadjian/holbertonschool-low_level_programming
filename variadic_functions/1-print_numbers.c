0f11cade91951e6e183d36a907107f9c1ce73aaa
Yadjian92 <10462@holbertonstudents.com>
2025-03-17 22:43:27 +0100

    Task0

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,1 +1,14 @@
-a
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - Sums all its parameters.
+* @n: The number of arguments passed to the function.
+* Return: The sum of all parameters, or 0 if n is 0.
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
