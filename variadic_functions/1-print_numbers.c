caf83985bef2f5438499cbee0d8e22aebfa9cfb2
Yadjian92 <10462@holbertonstudents.com>
2025-03-18 14:37:27 +0100

    Refactor variadic functions: improve print_numbers and add print_strings implementation

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,9 +11,17 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i;
 
-if (separator == NULL)
-printf(", ");
+	va_start(args, n);
 
-printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, unsigned int));
+			if (separator != NULL && i < n - 1)
+				printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
 }
