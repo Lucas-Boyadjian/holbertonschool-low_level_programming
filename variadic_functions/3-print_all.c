f06a9781b8f8e066d046a0ec15efa337c3bb1d19
Yadjian92 <10462@holbertonstudents.com>
2025-03-18 19:13:34 +0100

    Implement print_all function and enhance print_strings for NULL handling

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,1 +1,11 @@
-a
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* print_all - Prints anything based on the format provided.
+* @format: A list of types of arguments passed to the function.
+* Return: Nothing.
+*/
+
+void print_all(const char * const format, ...)
