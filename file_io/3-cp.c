497403fa465a712dbab96767da954cc16bb74a35
Yadjian92 <10462@holbertonstudents.com>
2025-04-02 14:58:55 +0200

    Refactor error handling in file copy functionality: rename error_handler to error_file, introduce close_files function, and improve error messages for clarity.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,74 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- * error_handler - Handles error messages and exits with appropriate code
- * @error_code: Error code to determine the message and exit status
- * @filename: Name of the file related to the error
+ * error_file - Handles file errors and exits with appropriate code
  * @fd_from: File descriptor for source file
  * @fd_to: File descriptor for destination file
+ * @argv: Array of arguments
+ * @error_code: Type of error
  */
-void error_handler(int error_code, char *filename, int fd_from, int fd_to)
+void error_file(int fd_from, int fd_to, char *argv[], int error_code)
 {
-	switch (error_code)
+	if (error_code == 98)
 	{
-	case 97:
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		if (fd_from != -1)
+			close(fd_from);
 		exit(98);
-	case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	}
+	else if (error_code == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		if (fd_from != -1)
 			close(fd_from);
 		if (fd_to != -1)
 			close(fd_to);
 		exit(99);
-	case 100:
+	}
+	else if (error_code == 100)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
-	default:
-		break;
 	}
 }
 
 /**
- * copy_file - Copies content from one file to another
- * @file_from: Source file name
- * @file_to: Destination file name
+ * close_files - Handles closing files and checking for errors
+ * @fd_from: File descriptor for source file
+ * @fd_to: File descriptor for destination file
  */
-void copy_file(char *file_from, char *file_to)
+void close_files(int fd_from, int fd_to)
 {
-	int fd_from, fd_to, close_status;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		error_handler(98, file_from, -1, -1);
-
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-		error_handler(99, file_to, fd_from, -1);
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_handler(99, file_to, fd_from, fd_to);
-	}
-
-	if (bytes_read == -1)
-		error_handler(98, file_from, fd_from, fd_to);
+	int close_status;
 
 	close_status = close(fd_from);
 	if (close_status == -1)
-		error_handler(100, NULL, fd_from, fd_to);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 
 	close_status = close(fd_to);
 	if (close_status == -1)
@@ -81,16 +66,44 @@ void copy_file(char *file_from, char *file_to)
 /**
  * main - Copies the content of a file to another file
  * @argc: Number of arguments
- * @argv: Array of argument strings
- *
- * Return: 0 on success
+ * @argv: Array of arguments
+ * Return: 0 on success, or exit with error code
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-		error_handler(97, NULL, -1, -1);
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-	copy_file(argv[1], argv[2]);
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_file(fd_from, -1, argv, 98);
+
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_file(fd_from, fd_to, argv, 99);
+
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+			error_file(fd_from, fd_to, argv, 99);
+	}
+
+
+	if (bytes_read == -1)
+		error_file(fd_from, fd_to, argv, 98);
+
+	close_files(fd_from, fd_to);
 
 	return (0);
 }
