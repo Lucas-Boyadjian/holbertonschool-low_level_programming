c5bc88ee4681edf3314403c27be3e6340612b2dd
Yadjian92 <10462@holbertonstudents.com>
2025-04-04 13:08:58 +0200

    Fix error messages in file copy: correct typos and improve error handling for read failures

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,25 +30,20 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read frome file %s\n", argv[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(file_to, buffer, bytes_read);
@@ -59,6 +54,12 @@ int main(int argc, char *argv[])
 			close(file_to);
 			exit(99);
 		}
+	}
+	if (bytes_read == -1)
+	{
+		close_file(file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	close_file(file_from);
 	close_file(file_to);
